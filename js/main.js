bdf5781f9e0d3639035fd67d891acb16f1d9a0be
Hugo <hugo@hotmail.co.uk>
2018-03-01 13:24:58 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2){
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-	}
+	alert("You found a match!");
+	} 
 	else {
-		alert("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
-}
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+};
+
+flipCard(1);
+flipCard(2);
+
